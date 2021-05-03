05c1aff616e31f9de968c5c47339b13b94e8db5c
LB <laurabradburn27@gmail.com>
2021-05-03 15:58:31 -0400

    initial commit- hello world

---
+++ lb-bb-site-v2/src/components/Navbar.js
@@ -0,0 +1,13 @@
+//rfce
+import React from "react";
+
+function Navbar() {
+  return <>
+  <div className="navbar">
+      <div className="navbar-container container">
+      </div>
+    </div>  
+  </>;
+}
+
+export default Navbar;
